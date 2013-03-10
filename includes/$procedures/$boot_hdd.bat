62b0b0e5ea69fe8bf0bc58286fbae39dec749cb7
Juby_PW <patryk.wegrzynski@gmail.com>
2013-03-10 17:57:41 +0100

    Modyfikacja VME dla Agent PenDrive'a. Modyfikacja dotyczy sposobu wykrywania partycji, indeksowania, bootowania i MBR. Cz. 9.

--- includes/$procedures/$boot_hdd.bat
+++ includes/$procedures/$boot_hdd.bat
@@ -1,15 +1,31 @@
 :HDD ---------------------------------------------------------------------------
-
-:: CHECK_HDD_EXISTING ----------------------------------------------------------
 CLS
+:: CHECK_HDD_EXISTING ----------------------------------------------------------
+:: IF EXIST "%1" RMDIR /Q /S "%1"
 :: IF EXIST "%1.vme_hdd" "includes\cmds\WinRAR\UNRAR" x -p[T(ANB;sWHyzDF.etqCohzFPQ5q;SbK3]VjYdoQ}]P{Pb}[4;LRmLcAgF?meg]Gs7?RJtnY;BcSss2VwtshD5xzr;k6mvsc2x_qrvPu)?L!ytZunSb!,9!tcqXWkP}LMGBCC6(PcXTaRz4CREHTtML5Bq-,EFM744qm{,}qZ9Zw8_{!MVXFpqr?ex_yDU6n;V8CWo_:V8y_g))[9bPW2F4ECbDW(qH}KvSojZqD{pMu.Z;aAS)gdf,wQFS3]ghVc}HUf{dt,?o,6rcnJp3:45yzrR2.)HWqXkJCy:(z_y,dqdY{2-A]c}[36b3jz!8Wpxfnzsn-romfZKu-R]B[JLbv,De,HMY)JDyJ(mR,8Bg{3bFymW-]pj?q,ksJ)QVWaUmmEBntT4-.DMz!q-}dqF4s?UACqceY)ZJSh]KBeRBykB:9JCL{Xu!4NFnDx2h]?-3pBt!wJGu[Xwhn{[.c;?FSL-bJ8TTeZ,3hu7dDu7MR4]DhDETHKefcnR,TqU{PWg(QNp9GV,e;(xnQCEd3zTEmqFEfgS[C}E93[Gp6H3aUZVqhf4Kf:6Zsrk78CgqR,Xy(mog[PQ4vsTd7:;M!9sxZc5)uK:LMZKXL)DU3a;W2WA{ZDZ6tS3S[)6:X8Pq{eMYr7ySG:voZ],!N2aFyCe-PJ]sxYh]D[9[g83:aWq}7rE.SvGR),X3K9TH{ASRK-2-f_[Q6z5CrqCHkZfw2CXssx}?{8XYe;6T)epjeT.5JCt_h8!Jr3AjZPpd,LCfPo}33;KFXKrn4jvLRzaG3q!-DumFE:,aqBBRE3djs46VMZnjYW??L])6!ysJ.tyr[AGPh.{a4j]vHxdnUksnv!Sz:JH;BGuLWBgEBuumc!fz:TnWCeH)kf)n!UrGhk8D.PYCHS:.Xj.2Co:V2L9K-m)(E:S,o?FpVk?kC[kgsmG]8K6}cDYj;cvJfNL3U7QvRWe97YQYKc9y:D)eXcW2NSr[mBbEo-AE5vuAUJek,Mh;7vh[7Eb.c2[Wjmh7txrWtkcM[Q(]ju{QpvYVAAVsh6PZQ()H[:m,oWNwtZ?cTE-HA7kQoEG5rb]GoG6xKQy)tM[msAWLU5aaAT?:eM86;hss!WpPtn3m}b6-r_TLd_,t3-wGU4sYLkQRFDn{?NzE!6LvdmGRuHJVQFjqqaPBTk8f;Tu[otPsXfoBZ[8USxvoDdd_N-Josw4vQW!CQT[u{x(e?k}Yh[L.o!_B)R;!N8zj[!,ZX(Uc_6R{Q6d)e!{3[FkGdpL?VLq6Qs]u:66]B,J77,kL?xuACdWw2p::As87jjZnN]w5(EAjF.]Mx;3}g;(Ht{K_!xepgDa[P7e2rvJ)vTYsX?-D]C3:Qs]VqqK7[zfX6T;WPN9Jo)AKDH5NZ(Rv;3RuRgZZfF;Lf_o[WWSJa99twDGVPdRSFoMc4sCJ)GbsPY,r.T}b-p4:-rYW5vvqjG3M}kbL?MDVrFLre23nQ(qX,Nz7JvV-B:UQo]huW,PNGgE,GnpdxdAy-A5Q]?XM_xjw_2q64DyrzCsG;fz.TAzZQ5v2aTtjQbeXC5_mo-boJ3[MH[NgJMFewBF7a{P!q[4-dcs2j4N_(WVT7pAvBWa{dvkM[}SyJ,?,ex4X2w6cT?]!uL.]oaJZ5_tdHKc7Mh9?fB4F{}x7E_c}TcKFM9mwhea!!MbR[KXq2-]fF_L2A;f9g!v_dg;VUeZY-BwA2T?_2AonJ}MYC_{y)[-rq(LKqspw_m)aECjmT68hm.G;_V,F5[(UEXk[Q9?XUD{eWwZv3)dM).QFWS2-2d3mBMv7,,]VFJAhEfdosRA!R6Ku;s.664RnvLSwN4R9s7Q)M_zDZ}Jxmarq2sU(UryPCRL6zhDsPa;gj;.HDJE;z]t2:McWDbsV-C?-.KyQz,(j?)?JVKMUVF,Y8d,rPtMV(S3)h23h3dZ8r7wLxJnS5oxC2y[wrVR:uz}cZgT_UzgK7dc4:Y,7MyquwyqoUGqNF!!nnYuGYb8]PKdRqf:Bdt(9r.VLGLe4RkQCJKZo.)yPXcmMRp5[gfj8gDy;6d "%1.vme_hdd">nul
-:: IF NOT EXIST "%1.vme_hdd" GOTO ERROR
+:: IF NOT EXIST "%1.vme_hdd" GOTO ERROR_VME-BOOT_HDD_0A-0006-001
 
 :: BOOT_HDD --------------------------------------------------------------------
 CLS
-FOR /F "eol=[ tokens=1,2 delims==" %%a in (%VME_DIR%\%%c\$mbr.vme_mbr) do IF %%a==BOOTLOADER CALL "%1\%%b" & PAUSE
-ECHO ---------------------------------------------------------------------------
+IF NOT EXIST "%1\$mbr.vme_mbr" GOTO ERROR_VME-BOOT_HDD_0A-00010-001
+FOR /F "eol=[ tokens=1,2 delims==" %%a in (%1\$mbr.vme_mbr) do IF %%a==BOOTLOADER CALL "%1\%%b"
 PAUSE
+GOTO END
 
-IF EXIST "%1\$mbr.vme_mbr" FOR /F "eol=[ tokens=1,2 delims==" %%a in (%1\$mbr.vme_mbr) do IF %%a==PRIMARY CALL "%1\%%b" & GOTO END
-IF NOT EXIST "%1\$mbr.vme_mbr" GOTO ERROR
+:ERROR_VME-BOOT_HDD_0A-0006-001 ------------------------------------------------
+CLS
+FOR /F "tokens=1,2 delims==" %%a in (includes\langs\%VME_LANG%.vme_lng) do IF %%a==ERROR_VME-BOOT_HDD_0A-0006-001 (
+  IF EXIST "includes\cmds\wbat250\WBAT.COM" CALL "includes\cmds\wbat250\WBAT" BOX %%b OK
+  IF NOT EXIST "includes\cmds\wbat250\WBAT.COM" ECHO %%b & PAUSE
+)
+GOTO END
+
+:ERROR_VME-BOOT_HDD_0A-0010-001 ------------------------------------------------
+CLS
+FOR /F "tokens=1,2 delims==" %%a in (includes\langs\%VME_LANG%.vme_lng) do IF %%a==ERROR_VME-BOOT_HDD_0A-0010-001 (
+  IF EXIST "includes\cmds\wbat250\WBAT.COM" CALL "includes\cmds\wbat250\WBAT" BOX %%b OK
+  IF NOT EXIST "includes\cmds\wbat250\WBAT.COM" ECHO %%b & PAUSE
+)
+GOTO END
+
+:END ---------------------------------------------------------------------------
