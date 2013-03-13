1d8a40857fbc8100f761d6ce16a89d7a5297e3a1
Agent PenDrive <administracja@agent-pendrive.c0.pl>
2013-03-13 22:07:17 +0100

    Zmiana com na mod w load_coms.

--- hda/hd0/boot/UFE_2.1.0169/includes/load_mods.bat
+++ hda/hd0/boot/UFE_2.1.0169/includes/load_mods.bat
@@ -1,5 +1,5 @@
 :LOAD_DEFAULTS -----------------------------------------------------------------
-DIR "%MODS%\*.ap_com" /b /a-d /one>"%RAM%\KERNEL\modules\$mods_list.ap_ini"
+DIR "%MODS%\*.ap_mod" /b /a-d /one>"%RAM%\KERNEL\modules\$mods_list.ap_ini"
 FOR /F "delims=" %%a in (%RAM%\KERNEL\modules\$mods_list.ap_ini) do CALL "%KERNEL_DIR%\includes\load.bat" "%%a"
 DEL "%RAM%\KERNEL\modules\$mods_list.ap_ini"
 
