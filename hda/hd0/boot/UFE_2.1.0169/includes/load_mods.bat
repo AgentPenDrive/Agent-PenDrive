d532130f98ff1a871a0cda76a7d0554b25f9c404
Agent PenDrive <administracja@agent-pendrive.c0.pl>
2013-03-13 23:12:22 +0100

    Mała pomyłka w load_mods.

--- hda/hd0/boot/UFE_2.1.0169/includes/load_mods.bat
+++ hda/hd0/boot/UFE_2.1.0169/includes/load_mods.bat
@@ -1,4 +1,4 @@
 :LOAD_DEFAULTS -----------------------------------------------------------------
-FOR /F "eol=[ tokens=1,2 delims==" %%a in (etc\boot\ufe.ap_conf) do IF %%a==MOD CALL "%KERNEL_DIR%\includes\load.bat" "%%a"
+FOR /F "eol=[ tokens=1,2 delims==" %%a in (etc\boot\ufe.ap_conf) do IF %%a==MOD CALL "%KERNEL_DIR%\includes\load.bat" "%%b"
 
 :END ---------------------------------------------------------------------------
