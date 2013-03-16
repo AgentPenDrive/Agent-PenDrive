35c098899c69fe5abf0c5d18a9766d8f7a579630
Agent PenDrive <administracja@agent-pendrive.c0.pl>
2013-03-16 15:07:32 +0100

    Zmiany

--- hda/hd0/lib/modules/src_code/lib/exec.bat
+++ hda/hd0/lib/modules/src_code/lib/exec.bat
@@ -8,18 +8,26 @@ GOTO END
 
 :DLL ---------------------------------------------------------------------------
 IF NOT EXIST "%RAM%\KERNEL\lib\dll" MD "%RAM%\KERNEL\lib\dll"
-IF %2==-clear_cache CALL "%COMDIR%\includes\dll\clear_cache.bat" %3 %4 %5 %6 %7 %8 %9
-IF %2==-kill_all CALL "%COMDIR%\includes\dll\kill_all.bat" %3 %4 %5 %6 %7 %8 %9
-IF %2==-load CALL "%COMDIR%\includes\dll\load.bat" %3 %4 %5 %6 %7 %8 %9
-IF %2==-load_defaults CALL "%COMDIR%\includes\dll\load_defaults.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-clear_cache CALL "%MODDIR%\includes\dll\clear_cache.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-kill_all CALL "%MODDIR%\includes\dll\kill_all.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-load CALL "%MODDIR%\includes\dll\load.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-load_defaults CALL "%MODDIR%\includes\dll\load_defaults.bat" %3 %4 %5 %6 %7 %8 %9
+GOTO END
+
+:SOL ---------------------------------------------------------------------------
+IF NOT EXIST "%RAM%\KERNEL\lib\sol" MD "%RAM%\KERNEL\lib\sol"
+IF %2==-clear_cache CALL "%MODDIR%\includes\sol\clear_cache.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-kill_all CALL "%MODDIR%\includes\sol\kill_all.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-load CALL "%MODDIR%\includes\sol\load.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-load_defaults CALL "%MODDIR%\includes\sol\load_defaults.bat" %3 %4 %5 %6 %7 %8 %9
 GOTO END
 
 :SYS ---------------------------------------------------------------------------
 IF NOT EXIST "%RAM%\KERNEL\lib\sys" MD "%RAM%\KERNEL\lib\sys"
-IF %2==-clear_cache CALL "%COMDIR%\includes\sys\clear_cache.bat" %3 %4 %5 %6 %7 %8 %9
-IF %2==-kill_all CALL "%COMDIR%\includes\sys\kill_all.bat" %3 %4 %5 %6 %7 %8 %9
-IF %2==-load CALL "%COMDIR%\includes\sys\load.bat" %3 %4 %5 %6 %7 %8 %9
-IF %2==-load_defaults CALL "%COMDIR%\includes\sys\load_defaults.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-clear_cache CALL "%MODDIR%\includes\sys\clear_cache.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-kill_all CALL "%MODDIR%\includes\sys\kill_all.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-load CALL "%MODDIR%\includes\sys\load.bat" %3 %4 %5 %6 %7 %8 %9
+IF %2==-load_defaults CALL "%MODDIR%\includes\sys\load_defaults.bat" %3 %4 %5 %6 %7 %8 %9
 GOTO END
 
 :END ---------------------------------------------------------------------------
