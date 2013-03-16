288d7a8ff8ad475f3f49fd95bcf97e0765ce124c
Agent PenDrive <administracja@agent-pendrive.c0.pl>
2013-03-16 13:38:34 +0100

    Zmiana nazwy modułu Var na Const.

--- hda/hd0/lib/modules/src_code/const/includes/clear.bat
+++ hda/hd0/lib/modules/src_code/const/includes/clear.bat
@@ -1,9 +1,9 @@
-IF EXIST "%RAM%\KERNEL\var\$act_var.ap_ini" GOTO EXIST
-IF NOT EXIST "%RAM%\KERNEL\var\$act_var.ap_ini" GOTO NOT_EXIST
+IF EXIST "%RAM%\KERNEL\const\$act_const.ap_ini" GOTO EXIST
+IF NOT EXIST "%RAM%\KERNEL\const\$act_const.ap_ini" GOTO NOT_EXIST
 
 :EXIST -------------------------------------------------------------------------
-FOR /F "eol=# tokens=1,2,3 delims==" %%a in (%RAM%\KERNEL\var\$act_var.ap_ini) do SET %%b=
-RMDIR /Q /S "%RAM%\KERNEL\var"
+FOR /F "eol=# tokens=1,2,3 delims==" %%a in (%RAM%\KERNEL\const\$act_const.ap_ini) do SET %%b=
+RMDIR /Q /S "%RAM%\KERNEL\const"
 GOTO END
 
 :NOT_EXIST ---------------------------------------------------------------------
