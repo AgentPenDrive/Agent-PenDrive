854eb8c30d115aff8ecc6bead6bb9e5f3783946b
Agent PenDrive <administracja@agent-pendrive.c0.pl>
2013-04-07 22:16:37 +0200

    Zmiana HAL i SAL, oraz małe zmiany.

--- hda/hd0/lib/drivers/src_code/sal/sal.bat
+++ hda/hd0/lib/drivers/src_code/sal/sal.bat
@@ -10,26 +10,26 @@
 :: SET COMMANDS_TYPE=%KERNEL_TYPE%\x86
 
 :: PROPER ----------------------------------------------------------------------
-IF NOT EXIST "%RAM%\KERNEL\hal" MD "%RAM%\KERNEL\hal"
+IF NOT EXIST "%RAM%\KERNEL\sal" MD "%RAM%\KERNEL\sal"
 
 :: FIND_KERNEL_VER -------------------------------------------------------------
-VER >"%RAM%\KERNEL\hal\sys_NT_ver.ap_ini"
+VER >"%RAM%\KERNEL\sal\sys_NT_ver.ap_ini"
 
 :: SET_NT_VER ------------------------------------------------------------------
-FOR /F "delims=" %%a in (%RAM%\KERNEL\hal\sys_NT_ver.ap_ini) do SET NT_VER=%%a
-DEL "%RAM%\KERNEL\hal\sys_NT_ver.ap_ini"
+FOR /F "delims=" %%a in (%RAM%\KERNEL\sal\sys_NT_ver.ap_ini) do SET NT_VER=%%a
+DEL "%RAM%\KERNEL\sal\sys_NT_ver.ap_ini"
 
 SET NT_VER=%NT_VER:~-9%
 SET NT_VER=%NT_VER:~0,-1%
 SET NT_VER=NT_%NT_VER%
 
 :: CHECK_EXISTING_DRIVER_AND_LOAD ----------------------------------------------
-IF EXIST "%SYS%\drivers\hal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" CALL "$lib" /sys -load "%SYS%\drivers\hal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys"
-IF NOT EXIST "%SYS%\drivers\hal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" (
-  WBAT Agent PenDrive Cannot find proper HAL driver. Try to use Windows Compatibility Troubleshooter to fix that problem, or download seperate HAL driver form Official Suppoort, in download section.
+IF EXIST "%SYS%\drivers\sal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" CALL "$lib" /sys -load "%SYS%\drivers\sal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys"
+IF NOT EXIST "%SYS%\drivers\sal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" (
+  WBAT Agent PenDrive Cannot find proper SAL driver. Try to use Windows Compatibility Troubleshooter to fix that problem, or download seperate SAL driver form Official Suppoort, in download section.
   PAUSE
   SET ERROR=1
 )
 
 :: END -------------------------------------------------------------------------
-RMDIR /Q /S "%RAM%\KERNEL\hal"
+RMDIR /Q /S "%RAM%\KERNEL\sal"
