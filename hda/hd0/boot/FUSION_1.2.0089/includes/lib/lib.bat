8c89d6f4b4b55b0de149dbe8926a4c50a2d08018
Agent PenDrive <administracja@agent-pendrive.c0.pl>
2013-04-07 22:17:23 +0200

    Zmiana w systemie bibliotek FUSION.

--- hda/hd0/boot/FUSION_1.2.0089/includes/lib/lib.bat
+++ hda/hd0/boot/FUSION_1.2.0089/includes/lib/lib.bat
@@ -1,14 +1,10 @@
-:: DLL -----------------------------------------------------------------------
+:PROPER ------------------------------------------------------------------------
+IF NOT EXIST "%RAM%\BOOT\lib" MD "%RAM%\BOOT\lib"
 
+:CHECK_LIBS --------------------------------------------------------------------
 FOR /F "eol=[ tokens=1,2,3,4 delims==" %%a in (etc\mods\lib\lib.ap_conf) do IF %%a==LIB (
-  IF %%b==DLL (
-    IF %%c==LOCAL CALL "%BOOT_DIR%\includes\lib\includes\dll.bat" /local %%d
-    IF %%c==GLOBAL CALL "%BOOT_DIR%\includes\lib\includes\dll.bat" /global %%d
-  )
-  IF %%b==SYS (
-    IF %%c==LOCAL CALL "%BOOT_DIR%\includes\lib\includes\sys.bat" /local %%d
-    IF %%c==GLOBAL CALL "%BOOT_DIR%\includes\lib\includes\sys.bat" /global %%d
-  )
+  IF %%b==DLL CALL "%BOOT_DIR%\includes\lib\includes\dll.bat" %%c %%d
+  IF %%b==SYS CALL "%BOOT_DIR%\includes\lib\includes\sys.bat" %%c %%d
 )
 
 :: -----------------------------------------------------------------------------
