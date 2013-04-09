3dc11f849bedec5416036fd7d83946ae526c6ddc
Agent PenDrive <administracja@agent-pendrive.c0.pl>
2013-04-09 14:15:10 +0200

    Naprawa SAL.

--- hda/hd0/lib/drivers/src_code/sal/sal.bat
+++ hda/hd0/lib/drivers/src_code/sal/sal.bat
@@ -13,7 +13,7 @@
 IF NOT EXIST "%RAM%\KERNEL\sal" MD "%RAM%\KERNEL\sal"
 
 :: FIND_KERNEL_VER -------------------------------------------------------------
-VER >"%RAM%\KERNEL\sal\sys_NT_ver.ap_ini"
+VER>"%RAM%\KERNEL\sal\sys_NT_ver.ap_ini"
 
 :: SET_NT_VER ------------------------------------------------------------------
 FOR /F "delims=" %%a in (%RAM%\KERNEL\sal\sys_NT_ver.ap_ini) do SET NT_VER=%%a
@@ -24,18 +24,17 @@ SET NT_VER=%NT_VER:~0,-1%
 SET NT_VER=NT_%NT_VER%
 
 :: CHECK_EXISTING_DRIVER_AND_LOAD ----------------------------------------------
-IF EXIST "lib\drivers\sal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" CALL "$lib" /sys -load "lib\drivers\sal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" & SET ERROR=0
-IF NOT EXIST "lib\drivers\sal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" (
-  FOR /F "tokens=1,2 delims==" %%a in (share\langs\lib\drivers\sal\%AP_LANG%.ap_lng) do IF %%a==ERROR_SAL_0A-0029 (
+IF EXIST "%LIB%\drivers\sal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" CALL "$lib" /sys -load "%LIB%\drivers\sal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" & SET ERROR=0
+IF NOT EXIST "%LIB%\drivers\sal\%NT_VER%_%PROCESSOR_ARCHITECTURE%.ap_sys" (
+  FOR /F "tokens=1,2 delims==" %%a in (%SHARE%\langs\lib\drivers\sal\%AP_LANG%.ap_lng) do IF %%a==ERROR_SAL_0A-0029 (
     IF "%PROCESSOR_ARCHITECTURE%"=="x86" (
-      IF EXIST "bin\cmds\wbat250\WBAT.COM" CALL "bin\cmds\wbat250\WBAT" BOX %%b OK
-      IF NOT EXIST "bin\cmds\wbat250\WBAT.COM" CLS & ECHO %%b & PAUSE
+      IF EXIST "%BIN%\cmds\wbat250\WBAT.COM" CALL "%BIN%\cmds\wbat250\WBAT" BOX %%b OK
+      IF NOT EXIST "%BIN%\cmds\wbat250\WBAT.COM" CLS & ECHO %%b & PAUSE
     )
     IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" CLS & ECHO %%b & PAUSE
   )
   SET ERROR=1
 )
-
 
 :: END -------------------------------------------------------------------------
 RMDIR /Q /S "%RAM%\KERNEL\sal"
