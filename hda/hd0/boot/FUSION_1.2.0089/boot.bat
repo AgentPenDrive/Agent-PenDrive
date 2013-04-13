37d4a1809757a407a7db4b996764aaa4db5591f6
Juby_PW <patryk.wegrzynski@gmail.com>
2013-04-13 20:17:49 +0200

    Zmiana zmiennej katalogu głownego z AP_DIR na ROOT.

--- hda/hd0/boot/FUSION_1.2.0089/boot.bat
+++ hda/hd0/boot/FUSION_1.2.0089/boot.bat
@@ -1,6 +1,6 @@
 :PROPER ------------------------------------------------------------------------
 CD "%VME_DIR%\%1\%2"
-SET AP_DIR=%VME_DIR%\%1\%2
+SET ROOT=%VME_DIR%\%1\%2
 FOR /F "tokens=1,2 delims==" %%a in (etc\boot\fusion.ap_conf) do IF %%a==DEF_LANG SET AP_LANG=%%b
 
 :CHECK -------------------------------------------------------------------------
