0fea32d7d06b6ba1a42bbaa9df304e94ec08def5
Juby_PW <patryk.wegrzynski@gmail.com>
2013-11-24 15:15:07 +0100

    Rozpoczęcie procesu dostosowania kodu do nowych standardów.

--- START.bat
+++ START.bat
@@ -1,36 +1,75 @@
-@Echo OFF
-CLS
-TITLE Agent PenDrive - v1.5
+:: ----------------------------------------------------------------------------+
+:: Copyright (C) 2009, 2013 GitSoft                                            :
+::                                                                             :
+:: This file is part of Agent PenDrive.                                        :
+::                                                                             :
+:: This program is free software: you can redistribute it and/or modify        :
+:: it under the terms of the GNU General Public License as published by        :
+:: the Free Software Foundation, either version 3 of the License, or           :
+:: (at your option) any later version.                                         :
+::                                                                             :
+:: This program is distributed in the hope that it will be useful,             :
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of              :
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               :
+:: GNU General Public License for more details.                                :
+::                                                                             :
+:: You should have received a copy of the GNU General Public License           :
+:: along with this program; if not, see <http://www.gnu.org/licenses/>.        :
+:: ----------------------------------------------------------------------------+
+:: Prawa autorskie (C) 2009, 2013 GitSoft                                      :
+::                                                                             :
+:: Ten plik jest cz©˜ci¥ Agent PenDrive.                                       :
+::                                                                             :
+:: Ten program jest wolnym oprogramowaniem; mo¾esz go rozprowadza† dalej       :
+:: i/lub modyfikowa† na warunkach Powszechnej Licencji Publicznej GNU,         :
+:: wydanej przez Fundacj© Wolnego Oprogramowania - wedˆug wersji 2 tej         :
+:: Licencji lub (wedˆug twojego wyboru) kt¢rej˜ z p¢«niejszych wersji.         :
+::                                                                             :
+:: Niniejszy program rozpowszechniany jest z nadziej¥, i¾ b©dzie on            :
+:: u¾yteczny - jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domy˜lnej             :
+:: gwarancji PRZYDATNO—CI HANDLOWEJ albo PRZYDATNO—CI DO OKRE—LONYCH           :
+:: ZASTOSOWAã. W celu uzyskania bli¾szych informacji si©gnij do                :
+:: Powszechnej Licencji Publicznej GNU.                                        :
+::                                                                             :
+:: Z pewno˜ci¥ wraz z niniejszym programem otrzymaˆe˜ te¾ egzemplarz           :
+:: Powszechnej Licencji Publicznej GNU (GNU General Public License);           :
+:: je˜li nie, zobacz <http://www.gnu.org/licenses/>.                           :
+:: -----------------------------------------------------------------------------
 
-:START
+:PROPER ========================================================================
+@ECHO OFF
+TITLE Agent PenDrive
 CLS
-echo.
-echo                                   JUBY
-echo                                 PRESENTS
-echo.
-echo                              Agent PenDrive
-echo                                   v1.5
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo Wybierz opcje...
-echo.
-echo	1.Uruchom program	2.Edytor	W.Wyjscie
-set /p"cho=>"
-if %cho%==0 "HIDEN FEATURES.BAT"
-if %cho%==1 "PASSWORD.BAT"
-if %cho%==2 GOTO EDIT
-if %cho%==w "STOP.BAT"
-if %cho%==W "STOP.BAT"
-CLS
-echo Blad!
-PAUSE
-GOTO START
+GOTO WELCOME_SCREEN
 
-:EDIT
+:WELCOME_SCREEN ================================================================
+CLS
+ECHO.
+ECHO                                  GitSoft
+ECHO                                Przedstawia:
+ECHO.
+ECHO                              Agent PenDrive
+ECHO                                  v1.5.1
+ECHO.
+ECHO.
+ECHO.
+ECHO.
+ECHO.
+ECHO.
+ECHO.
+ECHO.
+ECHO.
+ECHO	1.Logowanie	W.Wyjscie
+SET /P OPT=">"
+IF %OPT%=="0" CALL "FORGET_PASSWD.BAT"
+IF %OPT%=="1" CALL "LOGIN.BAT"
+IF %OPT%=="2" GOTO EDIT
+IF %OPT%=="w" GOTO END
+IF %OPT%=="W" GOTO END
+GOTO WELCOME_SCREEN
+
+:EDIT ==========================================================================
 TITLE Agent Pendrive - Edytor
-edit.COM
+CALL "edit.COM"
+
+:END ===========================================================================
