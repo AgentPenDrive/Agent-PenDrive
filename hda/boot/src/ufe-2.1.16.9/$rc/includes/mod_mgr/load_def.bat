ad7044554c7c36774f5abf2d28030934d294eed2
Juby_PW <patryk.wegrzynski@gmail.com>
2013-11-26 00:34:24 +0100

    C.D.

--- hda/boot/src/ufe-2.1.16.9/$rc/includes/mod_mgr/load_def.bat
+++
@@ -1,4 +0,0 @@
-:LOAD_DEFAULTS -----------------------------------------------------------------
-FOR /F "eol=[ tokens=1,2 delims==" %%a in (etc\boot\ufe.ap_conf) do IF %%a==MOD CALL "%KERNEL_DIR%\includes\load.bat" %%b
-
-:END ---------------------------------------------------------------------------
---
+++ hda/system/sys/coms/src_code/updater/includes/download.bat
@@ -0,0 +1,64 @@
+:: CREATE_FTP_TEMP -------------------------------------------------------------
+IF NOT EXIST "%AP_TMP%\SYSTEM\updater\download" MD "%AP_TMP%\SYSTEM\updater\download"    
+
+:: GET_CURRENT_VERSION_NR ------------------------------------------------------
+CALL "$reg" /load "KEY_LOCAL_CONFIG\ap_conf" "[MAIN]" "AP_VER" "AP_VER"
+
+:: CREATE_DOWNLOAD_FILE --------------------------------------------------------
+FOR /F "eol=# tokens=1,2,3,4,5,6,7 delims==" %%a in (ver.ap_inf) do IF %%a==1.6 (
+  IF "%%b" GTR "%AP_VER%" (
+    SET HOST=%%c
+    SET USER=%%d
+    SET PASS=%%e
+    SET UPD_PATH=%%f
+    SET FILE=%%g
+  )
+)
+
+:: CREATE_DOWNLOAD_FILE --------------------------------------------------------
+ECHO OPEN %HOST%>"%AP_TMP%\SYSTEM\updater\download\upd_dwn.ap_ftp"
+ECHO %USER%>>"%AP_TMP%\SYSTEM\updater\download\upd_dwn.ap_ftp" 
+ECHO %PASS%>>"%AP_TMP%\SYSTEM\updater\download\upd_dwn.ap_ftp"
+ECHO GET "%UPD_PATH%/%FILE%">>"%AP_TMP%\SYSTEM\updater\download\upd_dwn.ap_ftp"
+ECHO CLOSE>>"%AP_TMP%\SYSTEM\updater\download\upd_dwn.ap_ftp"
+ECHO QUIT>>"%AP_TMP%\SYSTEM\updater\download\upd_dwn.ap_ftp"
+
+PAUSE
+
+:: DOWNLOAD_UPDATE_FILE --------------------------------------------------------
+FTP -s:%AP_TMP%\SYSTEM\updater\download\upd_dwn.ap_ftp>nul
+PAUSE
+
+:: MOVE_TO_TEMP_FTP_FILE -------------------------------------------------------
+MOVE "%FILE%" "%AP_TMP%\SYSTEM\updater\download"     
+
+ECHO RUN UPDATE
+PAUSE
+:: RUN_UPDATE ------------------------------------------------------------------
+CALL "$run" %AP_TMP%\SYSTEM\updater\download\%FILE% -upd
+IF %ERROR%=1 GOTO UPDATE_FALSE
+IF %ERROR%=0 GOTO UPDATE_TRUE
+
+:: ERRORS ----------------------------------------------------------------------
+
+:UPDATE_FALSE
+ECHOC 0 10 An error has accured while trying to update!
+SET UPDATE_download=0
+SET ERROR=1
+GOTO END                      
+
+:UPDATE_TRUE
+ECHOC 0 13 Update Succesfully!
+SET UPDATE-download=1 
+SET ERROR=0
+GOTO END  
+    
+:: -----------------------------------------------------------------------------
+
+:END      
+RMDIR /Q /S "%AP_TMP%\SYSTEM\updater\download"
+SET HOST=
+SET USER=
+SET PASS=
+SET UPD_PATH=
+SET FILE=
