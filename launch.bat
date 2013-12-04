41bfc8592547ebf95483c59406cc87a48b5c9491
Juby_PW <patryk.wegrzynski@gmail.com>
2013-12-04 21:08:42 +0100

    Dodanie czysczenia RAMu na start, oraz ustawianie zmiennyej VME_DIR.

--- launch.bat
+++ launch.bat
@@ -2,8 +2,18 @@
 TITLE Agent PenDrive Launcher
 CLS
 
-7z e
-PAUSE
+:CLEAR_RAM =====================================================================
+CLS
+IF EXIST "RAM" RMDIR /Q /S "RAM"
+IF NOT EXIST "RAM" MKDIR "RAM"
+
+:SET_VAR =======================================================================
+SET VME_DIR=%CD%
+
+:BOOT ==========================================================================
+CLS
+7z x -oRAM -prfywYDhhU39wUxrQNNbNdzATzNct9CrYjpUHtCqM5YJqWAEbNgpsfpKfCn9bqYA9NUgFX7Ttw3hS3otg4g8J6hp85XJToJtA59GsF2kBu3TWPLChtasXSgwnDFFrPysk8zcdZWeUpygDKjnCTd2bTWWX34Nh2F3yEDb62QqQN7TXPF28jsHCN6ZgyeYZkrXqvzVVQLc4hwsxBT6mKeoYJNEHs48cUw2QwobLyNfgJ8eKrJvAojYww5EpydZT9s95 "hda\boot\fusion-0.1.2.8.ap_bin"
+CALL "RAM\fusion-0.1.2.8\exec.bat"
 
 :END ---------------------------------------------------------------------------
 CLS
